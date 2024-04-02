a8d3199eef7e742f8c38ae42286de001c2340a23
BetoAnh <vtahvp@gmail.com>
2024-04-02 14:14:30 +0700

    upadte

--- Components/Admin/Donate/DonateIndex.js
+++ Components/Admin/Donate/DonateIndex.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const sql = require("mssql");
 const dbConnection = require("../../../Config/dbConnection");
 
-
 router.get("/", async (req, res) => {
   try {
     await dbConnection();
@@ -18,7 +17,7 @@ router.get("/", async (req, res) => {
     const donates = donateResult.recordset;
 
     res.status(200).json({
-        donates: donates,
+      donates: donates,
     });
   } catch (error) {
     console.error(error);
@@ -43,7 +42,7 @@ router.get("/history/:donateid", async (req, res) => {
     const donates = donateResult.recordset;
 
     res.status(200).json({
-        donates: donates,
+      donates: donates,
     });
   } catch (error) {
     console.error(error);
@@ -67,7 +66,7 @@ router.get("/:donateid", async (req, res) => {
     const donates = donateResult.recordset;
 
     res.status(200).json({
-        donates: donates,
+      donates: donates,
     });
   } catch (error) {
     console.error(error);
@@ -75,5 +74,74 @@ router.get("/:donateid", async (req, res) => {
   }
 });
 
+router.post("/add", async (req, res) => {
+  const { img, donatename, description, price } = req.body;
+  try {
+    await dbConnection();
+    const pool = await sql.connect(dbConnection);
+    const request = pool.request();
+    const insertDonateQuery = `
+      INSERT INTO Donate (img, donatename, description, price)
+      VALUES (@img, @donatename, @description, @price)
+    `;
+    await request
+      .input("img", sql.NVarChar, img)
+      .input("donatename", sql.NVarChar, donatename)
+      .input("description", sql.NVarChar, description)
+      .input("price", sql.Float, price)
+      .query(insertDonateQuery);
+    res.status(201).json({ message: "Donate created successfully" });
+  } catch (error) {
+    console.error("Error creating Donate: ", error);
+    res.status(500).json({ message: "Error creating Donate" });
+  }
+});
+
+router.delete("/:donateid", async (req, res) => {
+  const { donateid } = req.params;
+  try {
+    await dbConnection();
+    const pool = await sql.connect(dbConnection);
+    const request = pool.request();
+    const deleteDonateQuery = `
+    DELETE FROM Donate
+    WHERE donateid = @donateId
+    `;
+    await request.input("donateId", sql.Int, donateid).query(deleteDonateQuery);
+    res.status(201).json({ message: "Donate deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting Donate: ", error);
+    res.status(500).json({ message: "Error deleting Donate" });
+  }
+});
+
+router.post("/edit/:donateid", async (req, res) => {
+  const { img, donatename, description, price } = req.body;
+  const { donateid } = req.params;
+  try {
+    await dbConnection();
+    const pool = await sql.connect(dbConnection);
+    const request = pool.request();
+    const editDonateQuery = `
+    UPDATE Donate
+      SET img = @img,
+          donatename = @donatename,
+          description = @description,
+          price = @price
+      WHERE donateid = @donateId
+    `;
+    await request
+      .input("img", sql.NVarChar, img)
+      .input("donatename", sql.NVarChar, donatename)
+      .input("description", sql.NVarChar, description)
+      .input("price", sql.Float, price)
+      .input("donateId", sql.Int, donateid)
+      .query(editDonateQuery);
+    res.status(201).json({ message: "Donate updated successfully" });
+  } catch (error) {
+    console.error("Error updating Donate: ", error);
+    res.status(500).json({ message: "Error updating Donate" });
+  }
+});
 
 module.exports = router;
